c96ee5ea62334154aca73fa555e340d2e71ec199
Yash Indane <yashindane46@gmail.com>
2021-06-07 22:12:54 +0530

    Update Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -1,5 +1,3 @@
-def qsort (L):#quick sort in one line
-    if L == []: return []
-    return qsort([x for x in L[ 1 :] if x< L[ 0 ]]) + L[ 0 : 1 ] + qsort([x for x in L[ 1 :] if x>=L[ 0 ]])
+def qsort (L) : return [] if L == [] else qsort([x for x in L[ 1 :] if x< L[ 0 ]]) + L[ 0 : 1 ] + qsort([x for x in L[ 1 :] if x>=L[ 0 ]])
 lst = [ 44 , 33 , 22 , 5 , 77 , 55 , 999 ]
 print(qsort(lst))
